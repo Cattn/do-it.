1aedcf83726204480034f9c998cdb95812aa79c0
Logan <Logan@cattn.dev>
2025-06-29 17:42:10 -0400

    start the game page & add animations

---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+
+</script>
+
+
+
+<div class="flex flex-row">
+    <div class="bg-primary-container h-screen mr-10 mt-10 w-1/3 pb-5 rounded-tr-3xl">
+        <div class="flex flex-col">
+            <h1 class="text-on-primary-fixed title-text">Multipliers</h1>
+        </div>
+        <hr class="border-on-primary-container border-2">
+    </div>
+
+
+    <div class="bg-secondary-container ml-10 mr-10 mt-10 w-1/3 pb-5 rounded-t-3xl items-center flex justify-center">
+        <div class="w-46 h-62 bg-secondary rounded-md mb-40 rotate-20 flex items-center justify-center drop-shadow-lg hover:shadow-xl hover:scale-105 active:shadow-none active:scale-95 transition-all duration-100">
+            <div class="rounded-full bg-primary-container w-14 h-14 flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24" class="text-on-primary-container">
+                    <path fill="currentColor" d="m12 13.4l-2.917 2.925q-.277.275-.704.275t-.704-.275q-.275-.275-.275-.7t.275-.7L10.6 12L7.675 9.108Q7.4 8.831 7.4 8.404t.275-.704q.275-.275.7-.275t.7.275L12 10.625L14.892 7.7q.277-.275.704-.275t.704.275q.3.3.3.713t-.3.687L13.375 12l2.925 2.917q.275.277.275.704t-.275.704q-.3.3-.712.3t-.688-.3z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="bg-secondary-container mt-10 w-1/3 pb-5 rounded-tl-3xl ml-10">
+        <div class="flex flex-col">
+            <h1 class="text-on-primary-fixed title-text">Shop</h1>
+        </div>
+        <hr class="border-on-secondary-container border-2">
+    </div>
+</div>
+
+
+<style>
+    :global(body) {
+        overflow: hidden;
+    }
+
+    .title-text {
+        font-size: 32px;
+        font-family: "Roboto Flex", sans-serif;
+        font-optical-sizing: 32;
+        font-weight: 900;
+        font-style: normal;
+        text-align: center;
+        font-variation-settings:
+            "slnt" 0,
+            "wdth" 151,
+            "GRAD" 0,
+            "XOPQ" 68,
+            "XTRA" 603,
+            "YOPQ" 79,
+            "YTAS" 750,
+            "YTDE" -203,
+            "YTFI" 738,
+            "YTLC" 514,
+            "YTUC" 712;
+    }
+</style>
